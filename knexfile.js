6d7ba67e561f9d39f5284eef5a2d5ee48ca71bbb
Sandravaphilips <lacasandra1@gmail.com>
2020-11-01 23:52:34 -0800

    configured knex

---
+++ knexfile.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    development: {
+      client: 'sqlite3',
+      connection: {
+        filename: './data/epicdev.db3'
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './data/migrations'
+      },
+      seeds: {
+        directory: './data/seeds'
+      }
+    },
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: './data/migrations',
+      },
+      seeds: {
+        directory: './data/seeds'
+      },
+      useNullAsDefault: true,
+    },
+  };
