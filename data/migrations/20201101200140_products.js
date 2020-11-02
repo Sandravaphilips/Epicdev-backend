c1e047fd57bcf57ab578bb94bf8b068748165eb4
Sandravaphilips <lacasandra1@gmail.com>
2020-11-01 23:53:18 -0800

    created needed tables for the database

---
+++ data/migrations/20201101200140_products.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('products', table => {
+        table.increments();
+        table.string('name').notNullable();
+        table.string('sku').unique().notNullable()
+        table.float('price').notNullable();
+        table.integer('supplierId').unsigned().notNullable().references('id').inTable('suppliers').onUpdate('CASCADE').onDelete('CASCADE');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('products');
+};
---
+++ data/migrations/20201101200159_suppliers.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('suppliers', table => {
+        table.increments();
+        table.string('firstName').notNullable();
+        table.string('lastName').notNullable()
+        table.string('location').notNullable();
+        table.string('contactInfo').notNullable();
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('suppliers');
+};
