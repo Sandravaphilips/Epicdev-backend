02cd6557615c647678ddf6b14f288e6f68e79d0f
Sandravaphilips <lacasandra1@gmail.com>
2020-11-02 01:43:32 -0800

    corrected mispelled table name

--- data/migrations/20201102004006_products.js
+++ data/migrations/20201102004006_products.js
@@ -5,7 +5,7 @@ exports.up = function(knex) {
         table.string('name').notNullable();
         table.string('sku').unique().notNullable()
         table.float('price').notNullable();
-        table.integer('supplierId').unsigned().notNullable().references('id').inTable('product.suppliers').onUpdate('CASCADE').onDelete('CASCADE');
+        table.integer('supplierId').unsigned().notNullable().references('id').inTable('products.suppliers').onUpdate('CASCADE').onDelete('CASCADE');
     });
 };
 
